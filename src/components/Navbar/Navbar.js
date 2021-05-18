30cabb780a6cee74bf5ecced6b0bf1087c26e47e
Anders Koo Furulund <anderskoo@live.no>
2021-05-18 15:03:59 +0200

    Update Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,23 +4,13 @@ import { Link } from "react-router-dom";
 import spacex from "./spacex.png";
 
 class Navbar extends Component {
-  state = { clicked: false };
-
-  handleClick = () => {
-    this.setState({ clicked: !this.state.clicked });
-  };
   render() {
     return (
       <nav className="navbarItems">
         <h1 className="navbar-logo">
           <img className="logo" src={spacex} alt="spacex" />
         </h1>
-        <div className="menu-icon" onClick={this.handleClick}>
-          <i
-            className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
-          ></i>
-        </div>
-        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+        <ul className="nav-menu">
           <Link className="nav-links underline" to="/" a href="/">
             Falcon 9
           </Link>
